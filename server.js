8b5a1c6a3ef095e88177a606d7d669124c40fca2
Sangram Ragad <sangram.r121@gmail.com>
2020-09-03 10:54:24 +0530

    removed initail route

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the contact keeper API" })
-);
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
